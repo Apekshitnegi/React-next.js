482fb81720174d3903da23e2dad069b3c01dc2ed
Apekshitnegi <apekshitnegi9@gmail.com>
2026-07-14 23:47:49 +0530

    Update QueueDisplay.jsx

--- 04_Queue_managment/src/components/QueueDisplay.jsx
+++ 04_Queue_managment/src/components/QueueDisplay.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 function QueueDisplay() {
+  const getStatusColor = (status) =>{
+    switch (status) {
+      case value "waiting":
+        return " var(--warning)";
+      case value "serving":
+        return " var(--Success)";
+      case value "completed":
+        return " var(--info)";
+    
+      default:
+        return "var(--text)"
+      
+    }
+  }
   return (
     <div>
-      <p>jhu</p>
+      QueueDisplay
     </div>
   )
 }
