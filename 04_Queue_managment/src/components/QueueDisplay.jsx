d649bb8ee8bafc6e1a4139d80ed60839b652aa50
Apekshitnegi <apekshitnegi9@gmail.com>
2026-07-20 23:01:52 +0530

    Update QueueDisplay.jsx

--- 04_Queue_managment/src/components/QueueDisplay.jsx
+++ 04_Queue_managment/src/components/QueueDisplay.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
+import React from "react";
 
-function QueueDisplay() {
-  const getStatusColor = (status) =>{
+function QueueDisplay({ queue, onUpdateStatus, onRemoveStatus }) {
+  const getStatusColor = (status) => {
     switch (status) {
-      case value "waiting":
-        return " var(--warning)";
-      case value "serving":
-        return " var(--Success)";
-      case value "completed":
-        return " var(--info)";
-    
+      case "waiting":
+        return "var(--warning)";
+
+      case "serving":
+        return "var(--success)";
+
+      case "completed":
+        return "var(--info)";
+
       default:
-        return "var(--text)"
-      
+        return "var(--text)";
     }
-  }
+  };
+
   return (
-    <div>
-      QueueDisplay
+    <div className="queue-display">
+      <h2>Queue Display</h2>
+
+      {queue.length === 0 ? (
+        <p className="empty-queue">No customer data</p>
+      ) : (
+        <div className="queue-list">
+          {
+            queue.map((customer) =>(
+              <div className="queue-item" key={customer.id}>
+                <div className="customer-info">
+                  <h3>{customer.name}</h3>
+                  <p>{customer.service}</p>
+                  <span className="status">
+                    style={{ color: getStatusColor(customer.status)}}
+
+                  </span>
+                </div>
+              </div>
+            )) }
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default QueueDisplay
+export default QueueDisplay;
